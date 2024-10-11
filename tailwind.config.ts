ed4fc978cf6d3286eeb00c729114b040cff2910a
timmie20 <olutimilehinolayinka1234@gmail.com>
2024-10-11 06:18:22 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,29 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "ease-in-quad": "(var(--ease-in-quad))",
+        "ease-in-cubic": "var(--ease-in-cubic)",
+        //   --ease-in-quart
+        //   --ease-in-quint
+        //   --ease-in-expo
+        //   --ease-in-circ
+
+        //   --ease-out-quad
+        //   --ease-out-cubic
+        //   --ease-out-quart
+        //   --ease-out-quint
+        //   --ease-out-expo
+        //   --ease-out-circ
+
+        //   --ease-in-out-quad
+        //   --ease-in-out-cubic
+        //   --ease-in-out-quart
+        //   --ease-in-out-quint
+        //   --ease-in-out-expo
+        //   --ease-in-out-circ
+        // }
+      },
     },
   },
   plugins: [],
